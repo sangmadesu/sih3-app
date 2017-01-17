d570b04bf52911a13a0d65620d5a731e428e4ac6
Adiyatma <adiyatma@Adiyatmas-MacBook-Pro.local>
2017-01-17 22:12:55 +0800

    new style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,34 @@
 
         <!-- Styles -->
         <style>
+
             body {
                 height: 100vh;
-                background-color: #24b2cc;
+                background-color: #f2f2f2;
                 display: flex;
                 flex-direction: columns;
                 justify-content: center;
                 align-items: center;
             }
-            .title {
-                color: white;
+            
+            h1 {
+                color: #333;
+                text-transform: uppercase;
+                font-size: 40px;
             }
+
+            .subtitle {
+                background-color: #edaa1a;
+                padding: 2px;
+                color: #333;
+                margin-left: 10px;
+                border-radius: 2px;
+            }
+
         </style>
     </head>
     <body>
-        <h1 class="title">Aplikasi Sistem SIH3</h1>
+        <h1>SIH3 BWS SUL III</h1>
+        <p class="subtitle">tahap development</p>
     </body>
 </html>
